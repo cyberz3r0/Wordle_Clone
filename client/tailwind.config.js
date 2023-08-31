f399f13f938b4b98b59c058f16e083421e443897
cyberz3r0 <reidsl0716@gmail.com>
2023-08-30 22:23:29 -0700

    trying to get flip animation

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,4 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
+
+// Rotate X utilities
+const rotateX = plugin(function ({ addUtilities }) {
+  addUtilities({
+    '.rotate-x-20': {
+      transform: 'rotateX(20deg)',
+    },
+    '.rotate-x-40': {
+      transform: 'rotateX(40deg)',
+    },
+    '.rotate-x-60': {
+      transform: 'rotateX(60deg)',
+    },
+    '.rotate-x-80': {
+      transform: 'rotateX(80deg)',
+    },
+  })
+})
+
 export default {
   content: [
     "./index.html",
@@ -7,6 +27,6 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [rotateX],
 }
 
